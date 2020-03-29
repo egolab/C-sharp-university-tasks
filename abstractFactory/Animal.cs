af39585aa224eac2129e58524bb005827f57743f
egolab <57237362+egolab@users.noreply.github.com>
2020-03-29 15:21:19 +0200

    add abstract factory

---
+++ abstractFactory/Animal.cs
@@ -0,0 +1,131 @@
+﻿namespace abstractFactory
+{
+    public class Animal
+    {
+        protected string color;
+        protected double weight;
+        protected int speed;
+ 
+        public virtual string ShowData()
+        {
+            return "Unspecified animal";
+        }
+    }
+
+    public class Dog: Animal
+    {
+        public Dog(string c, double w, int s)
+        {
+            color = c;
+            weight = w;
+            speed = s;
+        }
+
+        public override string ShowData()
+        {
+            return "Dog";
+        }
+
+        public override string ToString()
+        {
+            return "Dog\nColor: " + color + "\nWeight: " + weight + "\nSpeed: " + speed;
+        }
+    }
+
+    public class Lion : Animal
+    {
+        public Lion(string c, double w, int s)
+        {
+            color = c;
+            weight = w;
+            speed = s;
+        }
+
+        public override string ShowData()
+        {
+            return "Lion";
+        }
+
+        public override string ToString()
+        {
+            return "Lion\nColor: " + color + "\nWeight: " + weight + "\nSpeed: " + speed;
+        }
+    }
+
+    public class Falcon : Animal
+    {
+        public Falcon(string c, double w, int s)
+        {
+            color = c;
+            weight = w;
+            speed = s;
+        }
+        public override string ShowData()
+        {
+            return "Falcon";
+        }
+
+        public override string ToString()
+        {
+            return "Falcon\nColor: " + color + "\nWeight: " + weight + "\nSpeed: " + speed;
+        }
+    }
+
+    public class Parrot : Animal
+    {
+        public Parrot(string c, double w, int s)
+        {
+            color = c;
+            weight = w;
+            speed = s;
+        }
+
+        public override string ShowData()
+        {
+            return "Parrot";
+        }
+        public override string ToString()
+        {
+            return "Parrot\nColor: " + color + "\nWeight: " + weight + "\nSpeed: " + speed;
+        }
+    }
+
+    public class Shark : Animal
+    {
+        public Shark(string c, double w, int s)
+        {
+            color = c;
+            weight = w;
+            speed = s;
+        }
+
+        public override string ShowData()
+        {
+            return "Shark";
+        }
+        public override string ToString()
+        {
+            return "Lion\nColor: " + color + "\nWeight: " + weight + "\nSpeed: " + speed;
+        }
+    }
+
+    public class AquariumFish : Animal
+    {
+        public AquariumFish(string c, double w, int s)
+        {
+            color = c;
+            weight = w;
+            speed = s;
+        }
+
+        public override string ShowData()
+        {
+            return "Aquarium fish";
+        }
+
+        public override string ToString()
+        {
+            return "Aquarium Fish\nColor: " + color + "\nWeight: " + weight + "\nSpeed: " + speed;
+        }
+    }
+}
---
+++ abstractFactory/IAnimalFactory.cs
@@ -0,0 +1,9 @@
+﻿namespace abstractFactory
+{
+    public interface IAnimalFactory
+    {
+        abstract Animal CreateDomesticAnimal();
+        abstract Animal CreateWildAnimal();
+        abstract Animal Create(string type, string color, double weight, int speed);
+    }
+}
