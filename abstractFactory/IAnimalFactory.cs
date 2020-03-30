9554722794ce92905b347a548755bae52bd71164
egolab <57237362+egolab@users.noreply.github.com>
2020-03-30 18:18:52 +0200

    abstract factory update

--- abstractFactory/IAnimalFactory.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace abstractFactory
-{
-    public interface IAnimalFactory
-    {
-        abstract Animal CreateDomesticAnimal();
-        abstract Animal CreateWildAnimal();
-        abstract Animal Create(string type, string color, double weight, int speed);
-    }
-}
